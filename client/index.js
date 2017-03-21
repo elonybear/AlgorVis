975410df26d412db9c1871ccbb507ccf22a1fd65
Elon Rubin <emrubi@umich.edu>
2017-03-21 13:42:34 -0400

    setup, hello world

---
+++ client/index.js
@@ -0,0 +1,12 @@
+//libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//vendors
+import 'bootstrap';
+import './js/vendors/jquery-ui.min.js';
+
+ReactDOM.render(
+  <div>Hello World</div>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: __dirname + '/client/index.js',
+  output: {
+    path:__dirname + '/public',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    preLoaders: [
+      {test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/},
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css?sourceMap', 'sass?sourceMap']
+      }
+    ]
+  },
+
+  eslint: {
+    configFile: './.eslintrc'
+  },
+
+  plugins :[
+    new HtmlWebpackPlugin({
+      template: __dirname + '/client/index.tmpl.html',
+      inject: true
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      JQuery: 'jquery'
+    })
+  ],
+
+  devServer: {
+    contentBase: './public',
+    colors: true,
+    historyApiFallback: true,
+    inline: true,
+    port: 3000,
+    hot: true
+  }
+}
